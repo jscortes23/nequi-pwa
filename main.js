b23f49bddaddd86b9fbcc63ed12d465623f5b55e
Joan <js.199911@gmail.com>
2023-04-10 13:17:21 -0500

    Test view

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ const viewWithDestiny = $('.view-with-destiny')
 const viewFoundTaxi = $('.view-found-taxi')
 const viewIHaveArrived = $('.view-i-have-arrived')
 const viewCancelTrip = $('.view-cancel-trip')
+const viewOptionCancelTrip= $('.view-option-cancel-trip')
+const viewCancelTripSuccessful = $('.view-cancel-trip__successful')
+const viewRouteDriverInfo = $('.view-route-driver-info')
+const viewFinishedTrip = $('.view-finished-trip')
+const viewEcoTaxi = $('.view-eco-taxi')
 
 /* Buttons */
 const btnGo = $('.view-start .btn-lg-xxx')
@@ -23,6 +28,11 @@ const btnOrderTaxi= $('.view-route .btn-lg-xxx.btn-green')
 const btnCancelTaxi= $('.view-with-destiny .btn-alternative')
 const btnCloseInfo= $('.view-found-taxi .btn.btn-green.p-14-18')
 const btnFoundTaxi= $('.view-i-have-arrived .btn.btn-sm.btn-green')
+const btnOrderAfter= $('.view-cancel-trip .btn.btn-sm.btn-green')
+const btnCancelTrip= $('.view-option-cancel-trip .btn.btn-green')
+const btnNextView= $('.view-cancel-trip__successful .btn')
+const btnCloseInfoTaxi= $('.view-route-driver-info .btn.btn-green.p-14-18')
+const btnBack= $('.view-finished-trip .btn')
 
 function nextView(viewCurrent, viewNext) {
     viewCurrent.classList.add('hidden')
@@ -67,4 +77,24 @@ btnCloseInfo.addEventListener('click', () => {
 
 btnFoundTaxi.addEventListener('click', () => {
     nextView(viewIHaveArrived, viewCancelTrip)
+})
+
+btnOrderAfter.addEventListener('click', () => {
+    nextView(viewCancelTrip, viewOptionCancelTrip)
+})
+
+btnCancelTrip.addEventListener('click', () => {
+    nextView(viewOptionCancelTrip, viewCancelTripSuccessful)
+})
+
+btnNextView.addEventListener('click', () => {
+    nextView(viewCancelTripSuccessful, viewRouteDriverInfo)
+})
+
+btnCloseInfoTaxi.addEventListener('click', () => {
+    nextView(viewRouteDriverInfo, viewFinishedTrip)
+})
+
+btnBack.addEventListener('click', () => {
+    nextView(viewFinishedTrip, viewEcoTaxi)
 })
